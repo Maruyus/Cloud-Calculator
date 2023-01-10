a4399a09edbcf44580d2f20d78f71661814b570f
akkar <cmariusdaub@hotmail.com>
2023-01-10 11:35:04 +0100

    add pricing api strings

--- src/constants/CloudProviders.ts
+++ src/constants/CloudProviders.ts
@@ -1,7 +1,14 @@
 export enum CLOUD_PROVIDERS {
     AWS,
     Azure,
-    GCP,
     Alibaba,
     Oracle
+}
+
+export enum PRICING_API_URIS {
+    AWSQuery = "https://api.pricing.us-east-1.amazonaws.com",
+    AWSBulk = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json",
+    Azure = "https://prices.azure.com/api/retail/prices",
+    Alibaba = "https://business.ap-southeast-1.aliyuncs.com/?Action=GetSubscriptionPrice",
+    Oracle = "https://apexapps.oracle.com/pls/apex/cetools/api/v1/products/"
 }
