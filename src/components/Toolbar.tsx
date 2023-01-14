871a964b7f9520470bbd30d04023a9df27fa163f
akkar <cmariusdaub@hotmail.com>
2023-01-14 20:41:16 +0100

    toolbar wip

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -4,7 +4,13 @@ import App from '../App';
 
 class Toolbar extends React.Component {
     render() {
-        return <h1>Toolbar goes here</h1>;
+        return (
+            <div className="Toolbar">
+                <div className='space-y-4 w-100 bg-orange-400 shadow'>
+                    <h1>Toolbar goes here</h1>
+                </div>
+            </div>
+        )
     }
 }
 
