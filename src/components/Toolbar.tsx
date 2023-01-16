e0c93ceaee44d23f52198ad55938bdc465c48f4e
akkar <cmariusdaub@hotmail.com>
2023-01-16 20:34:10 +0100

    add toolbar items

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -6,11 +6,19 @@ class Toolbar extends React.Component {
     render() {
         return (
             <div className="w-100 bg-emerald-300 position-fixed top-0 bottom-0 w-100 shadow-md h-12 block">
-                <div className="space-y-4 max-w-7xl mx-auto px-5 h-12 grid items-center">
-                    <div className='flex '>
-                        <h1>Toolbar goes here</h1>
-                    </div>
-                </div>
+                <nav className="space-y-4 max-w-7xl mx-auto px-5 h-12 flex flex-row items-center">
+                    <ul className='flex space-x-8'>
+                        <li className='flex'>
+                            <a>Home</a>
+                        </li>
+                        <li className='flex'>
+                            <a>History</a>
+                        </li>
+                        <li className='flex'>
+                            <a>About</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         )
     }
